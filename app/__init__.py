da02743cbc2048b0f74df20b2979f3573d991e2e
Gen Ohta <genohta@Gens-MacBook-Air.local>
2020-08-16 17:08:06 -0700

    add migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from .config import Configuration
 from .models import db
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def main_page():
