411d229f05dbad2f071f64cfc9deb5ab02f76bb2
Gen Ohta <myself@genohta.com>
2020-08-18 10:32:16 -0700

    created a protected route

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,12 @@ from .models import db
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 
-from .routes import auth
+from .routes import auth, dashboard
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 app.register_blueprint(auth.bp)
+app.register_blueprint(dashboard.bp)
 jwt = JWTManager(app)
 db.init_app(app)
 migrate = Migrate(app, db)
