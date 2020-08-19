5e80857a4e74e58a35a73e8efe2ecf2a1a9788fe
Gen Ohta <myself@genohta.com>
2020-08-18 20:24:29 -0700

    use bcrypt

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from .config import Configuration
 from .models import db
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
 
 from .routes import auth, dashboard
 
@@ -11,6 +12,7 @@ app.config.from_object(Configuration)
 app.register_blueprint(auth.bp)
 app.register_blueprint(dashboard.bp)
 jwt = JWTManager(app)
+Bcrypt().init_app(app)
 db.init_app(app)
 migrate = Migrate(app, db)
 
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, request
 from flask_jwt_extended import create_access_token
 
+# from flask_bcrypt import generate_password_hash
+from flask_bcrypt import check_password_hash
+
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/login', methods=['POST'])
@@ -9,7 +12,9 @@ def login_access_token():
     email = body['email']
     password = body['password']
 
-    if email != 'test@test.com' and password != 'password':
+    # pw_hash = generate_password_hash('password')
+    pw_hash = b'$2b$12$nbcxvcyEYBLJoB0FgaBt3ee.pleTJNBJ5vkpccKNhq7fflzkiVyCq'
+    if email != 'test@test.com' and check_password_hash(pw_hash, 'password'):
         return { "message": "Bad username or password" }, 401
 
     access_token = create_access_token(identity=email)
