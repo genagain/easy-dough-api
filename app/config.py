bae7cc8cfdeb50808c9b2e864ec542e63249c924
Gen Ohta <myself@genohta.com>
2020-08-20 09:41:27 -0700

    write to test database when running tests

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Configuration():
     SECRET_KEY = os.environ['SECRET_KEY']
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ['TEST_DATABASE_URL']  if os.environ['FLASK_ENV'] == 'test' else  os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
