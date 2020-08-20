b58dd561f302f05955130a2744891356eda42095
Gen Ohta <myself@genohta.com>
2020-08-20 08:39:33 -0700

    add unique email test

--- app/tests/models/test_user.py
+++ app/tests/models/test_user.py
@@ -1,6 +1,7 @@
 import pytest
+from sqlalchemy.exc import IntegrityError
 
-from app.models import db
+from app import db
 from app.models.user import User
 
 from .utils import context
@@ -18,3 +19,23 @@ def test_add_user(context):
     assert user.lastname == 'User'
     assert user.email == 'test@test.com'
     assert user.password == 'password'
+
+def test_unique_email(context):
+    user = User(
+            firstname='Test',
+            lastname='User',
+            email='test@test.com',
+            password='password'
+            )
+    db.session.add(user)
+    db.session.commit()
+
+    user1 = User(
+            firstname='John',
+            lastname='Doe',
+            email='test@test.com',
+            password='test'
+            )
+    db.session.add(user1)
+    with pytest.raises(IntegrityError) as error:
+      db.session.commit()
