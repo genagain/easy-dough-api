5e4a062390bdf2cb4e9b911c3f7c4d521ebd6c35
Gen Ohta <myself@genohta.com>
2020-08-19 22:08:52 -0700

    made add user test idempotent

--- app/tests/models/test_user.py
+++ app/tests/models/test_user.py
@@ -3,20 +3,18 @@ import pytest
 from app.models import db
 from app.models.user import User
 
-from app import create_app
+from .utils import context
 
-def test_add_user():
-    app = create_app()
-    with app.app_context():
-        user = User(
-                firstname='Test',
-                lastname='User',
-                email='test@test.com',
-                password='password'
-                )
-        db.session.add(user)
-        db.session.commit()
-        assert user.firstname == 'Test'
-        assert user.lastname == 'User'
-        assert user.email == 'test@test.com'
-        assert user.password == 'password'
+def test_add_user(context):
+    user = User(
+            firstname='Test',
+            lastname='User',
+            email='test@test.com',
+            password='password'
+            )
+    db.session.add(user)
+    db.session.commit()
+    assert user.firstname == 'Test'
+    assert user.lastname == 'User'
+    assert user.email == 'test@test.com'
+    assert user.password == 'password'
---
+++ app/tests/models/utils.py
@@ -0,0 +1,15 @@
+import os
+
+import pytest
+
+from app import create_app, db
+
+@pytest.fixture
+def context():
+    app = create_app()
+    with app.app_context() as context:
+        db.session.remove()
+        db.drop_all()
+        db.create_all()
+        yield context
+
