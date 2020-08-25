cab904b533a1a3bcb88844281d32a00184a3ee2c
Gen Ohta <myself@genohta.com>
2020-08-25 09:18:39 -0700

    add invalid login test

--- app/tests/routes/test_auth.py
+++ app/tests/routes/test_auth.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import decode_token
 
 from .utils import client
 
-def test_login(client):
+def test_valid_login(client):
     response = client.post('/auth/login', json={
         'email': 'test@test.com',
         'password': 'password'
@@ -14,3 +14,11 @@ def test_login(client):
     decoded_token = decode_token(access_token)
     assert decoded_token['identity'] == 'test@test.com'
 
+def test_invalid_login(client):
+    response = client.post('/auth/login', json={
+        'email': 'test@test.com',
+        'password': 'bad password'
+        })
+    json_response = response.get_json()
+    message = json_response['message']
+    assert message == "Bad email or password"
