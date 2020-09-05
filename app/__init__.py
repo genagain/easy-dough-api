b14419c698787afffa55ed4c541a239827e4a1b7
Gen Ohta <myself@genohta.com>
2020-09-05 16:02:51 -0700

    remove unsed import

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from .config import Configuration
-# from .models import db
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
