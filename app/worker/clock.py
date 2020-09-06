cb3f3333a8d97db3166d57eaa674c7033906ad12
Gen Ohta <myself@genohta.com>
2020-09-06 15:44:30 -0700

    add users

--- app/worker/clock.py
+++ app/worker/clock.py
@@ -6,17 +6,33 @@ intervals.
 from datetime import datetime
 import time
 import os
+import random
+import string
 
+from flask_bcrypt import generate_password_hash
 from apscheduler.schedulers.background import BackgroundScheduler
 
+from app import db
+from app.models.user import User
 
 def tick():
+    hashed_password = generate_password_hash('test_password').decode('utf-8')
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(5))
+    user = User(
+            firstname='Random',
+            lastname='Test',
+            email=f"{result_str@test.com",
+            password=hashed_password
+            )
+    db.session.add(user)
+    db.session.commit()
     print('Tick! The time is: %s' % datetime.now())
 
 
 if __name__ == '__main__':
     scheduler = BackgroundScheduler()
-    scheduler.add_job(tick, 'interval', seconds=3)
+    scheduler.add_job(tick, 'interval', seconds=10)
     scheduler.start()
     print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
 
