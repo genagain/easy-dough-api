e97aaf7aa573504c84fd423c21a9ec36b17ea1bd
Gen Ohta <myself@genohta.com>
2020-09-06 14:54:49 -0700

    try quick start

--- worker/clock.py
+++ worker/clock.py
@@ -1,10 +1,29 @@
-# from apscheduler.schedulers.blocking import BlockingScheduler
+"""
+Demonstrates how to use the background scheduler to schedule a job that executes on 3 second
+intervals.
+"""
 
-# sched = BlockingScheduler()
+from datetime import datetime
+import time
+import os
 
-# @sched.scheduled_job('interval', minutes=3)
-# def timed_job():
-    # print('This job is run every three minutes.')
+from apscheduler.schedulers.background import BackgroundScheduler
 
-print('does this print')
-# sched.start()
+
+def tick():
+    print('Tick! The time is: %s' % datetime.now())
+
+
+if __name__ == '__main__':
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(tick, 'interval', seconds=3)
+    scheduler.start()
+    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
+    try:
+        # This is here to simulate application activity (which keeps the main thread alive).
+        while True:
+            time.sleep(2)
+    except (KeyboardInterrupt, SystemExit):
+        # Not strictly necessary if daemonic mode is enabled but should be done if possible
+        scheduler.shutdown()
