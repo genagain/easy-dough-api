c2a35479cb6a4e0507dbebc2eaff80db46c3b364
Gen Ohta <myself@genohta.com>
2020-09-06 14:16:37 -0700

    create dummy job

---
+++ clock.py
@@ -0,0 +1,9 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=3)
+def timed_job():
+    print('This job is run every three minutes.')
+
+sched.start()
