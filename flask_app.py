808c6bf566660e4ba79671df4f897892a5e55933
Gen Ohta <myself@genohta.com>
2020-09-06 17:12:42 -0700

    clean up imports

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 
-from app import create_app
-from app import scheduler
+from app import create_app, scheduler
+from app.models.user import User
+
 app = create_app()
 
 def tick():
     print('Tick! The time is: %s' % datetime.now())
     with app.app_context():
-        from app.models.user import User
         print(User.query.all())
 
 scheduler.add_job(tick, 'interval', seconds=10)
