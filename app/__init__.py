28b6652012aef8e1f972cb7b0805159f7231bfda
Gen Ohta <myself@genohta.com>
2020-09-14 13:27:48 -0700

    using blocking scheduler instead

--- app/__init__.py
+++ app/__init__.py
@@ -7,13 +7,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 db = SQLAlchemy()
 migrate = Migrate()
 jwt = JWTManager()
 bcrypt = Bcrypt()
-scheduler = BackgroundScheduler()
+scheduler = BlockingScheduler()
 
 def create_app():
     app = Flask(__name__)
