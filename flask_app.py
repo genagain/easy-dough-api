858e259dcf1c9d5cb1908f4b5d15633fe7d6874a
Gen Ohta <myself@genohta.com>
2020-09-14 15:05:42 -0700

    ingest transctions

--- flask_app.py
+++ flask_app.py
@@ -7,7 +7,7 @@ from flask_bcrypt import generate_password_hash
 from plaid import Client
 
 from app import create_app, db, scheduler
-from app.models import User
+from app.models import User, Transaction
 
 
 app = create_app()
@@ -32,12 +32,28 @@ def print_transactions():
     PLAID_CLIENT_ID = os.getenv('PLAID_CLIENT_ID')
     PLAID_SECRET = os.getenv('PLAID_SECRET')
     PLAID_ENV = os.getenv('PLAID_ENV', 'sandbox')
+    ## TODO make sure access tokens persisted at the user/account level
     access_token = os.getenv('ACCESS_TOKEN')
     client = Client(client_id=PLAID_CLIENT_ID, secret=PLAID_SECRET, environment=PLAID_ENV, api_version='2019-05-29')
+    # TODO change this to a day's worth of transactions once I have live data
     start_date = '{:%Y-%m-%d}'.format(datetime.now() + timedelta(-30))
     end_date = '{:%Y-%m-%d}'.format(datetime.now())
     transactions_response = client.Transactions.get(access_token, start_date, end_date)
-    print(transactions_response['accounts'])
+    transactions_data = transactions_response['transactions']
+    with app.app_context():
+        for transaction_datum in transactions_data:
+            date = transaction_datum['date']
+            description = transaction_datum['name']
+            amount = int(transaction_datum['amount']) * 100
+            transaction = Transaction(
+                           date=date,
+                           description=description,
+                           amount=amount
+                           )
+            db.session.add(transaction)
+            db.session.commit()
+
+
 
 # scheduler.add_job(add_user, 'cron', hour=1, minute=15)
 scheduler.add_job(print_transactions, 'interval', minutes=2)
