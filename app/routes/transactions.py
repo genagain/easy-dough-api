393fdb3bdfc4801831102a1582046d8dbe45e373
Gen Ohta <myself@genohta.com>
2020-09-22 20:28:30 -0700

    return transactions over date range

--- app/routes/transactions.py
+++ app/routes/transactions.py
@@ -1,5 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
+
+from ..models import Transaction
 
 bp = Blueprint('transactions', __name__, url_prefix='/transactions')
 
@@ -7,4 +9,13 @@ bp = Blueprint('transactions', __name__, url_prefix='/transactions')
 @jwt_required
 def transactions():
     current_user_email = get_jwt_identity()
-    return { 'message': 'Query parameters not found. Please provide dates or a search term in the request' }, 200
+    if request.args == {}:
+        return { 'message': 'Query parameters not found. Please provide dates or a search term in the request' }, 200
+
+    start_date = request.args.get('start_date')
+    end_date = request.args.get('end_date')
+
+    transactions = Transaction.query.filter(Transaction.date.between(start_date, end_date)).all()
+
+    transactions_data = list(map(lambda t: t.to_dict(), transactions))
+    return { 'transactions': transactions_data }, 200
--- app/tests/routes/test_transactions.py
+++ app/tests/routes/test_transactions.py
@@ -2,7 +2,7 @@ import pytest
 
 from flask_bcrypt import generate_password_hash
 from app import db
-from app.models import User
+from app.models import User, Transaction
 from .utils import client
 
 def test_unauthorized_transactions(client):
@@ -32,3 +32,34 @@ def test_transactions_no_query_params(client):
     json_response = response.get_json()
     assert json_response["message"] == 'Query parameters not found. Please provide dates or a search term in the request'
 
+def test_transactions_date_range(client):
+    hashed_password = generate_password_hash('password').decode('utf-8')
+    user = User(
+            firstname='Justin',
+            lastname='Test',
+            email='justin@test.com',
+            password=hashed_password
+            )
+    db.session.add(user)
+    db.session.commit()
+
+    may_transaction = Transaction(date='2020-05-15', description='Mexican place', amount=1500)
+    june_transaction = Transaction(date='2020-06-21', description='Italian restaurant', amount=2700)
+    july_transaction = Transaction(date='2020-07-04', description='BBQ', amount=4000)
+
+    db.session.add(may_transaction)
+    db.session.add(june_transaction)
+    db.session.add(july_transaction)
+    db.session.commit()
+
+    login_response = client.post('/auth/login', json={
+        'email': 'jessica@test.com',
+        'password': 'password'
+        })
+    json_login_response = login_response.get_json()
+    access_token = json_login_response['access_token']
+
+    response = client.get('/transactions?start_date=2020-06-01&end_date=2020-06-30', headers={ "Authorization": f"Bearer {access_token}" })
+    json_response = response.get_json()
+
+    assert [{ 'date': '2020-06-21', 'description': 'Italian restaurant', 'amount': 2700} ] == json_response['transactions']
