33e7b9a4cdd289a6c3ca3bf7b2065ca941dc7e3f
Gen Ohta <myself@genohta.com>
2020-09-22 19:12:17 -0700

    change dashboard blueprint to transactions endpoint

--- app/routes/transactions.py
+++ app/routes/transactions.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
-bp = Blueprint('dashboard', __name__, url_prefix='/dashboard')
+bp = Blueprint('transactions', __name__, url_prefix='/transactions')
 
 @bp.route('/', methods=['GET'], strict_slashes=False)
 @jwt_required
-def dashboard():
+def transactions():
     current_user_email = get_jwt_identity()
     return { 'logged_in_as': current_user_email }, 200
--- app/tests/routes/test_transactions.py
+++ app/tests/routes/test_transactions.py
@@ -5,12 +5,12 @@ from app import db
 from app.models import User
 from .utils import client
 
-def test_unauthorized_dashboard(client):
-    response = client.get('/dashboard')
+def test_unauthorized_transactions(client):
+    response = client.get('/transactions')
     json_response = response.get_json()
     assert json_response['msg'] == 'Missing Authorization Header'
 
-def test_authorized_dashboard(client):
+def test_authorized_transactions(client):
     hashed_password = generate_password_hash('password').decode('utf-8')
     user = User(
             firstname='Jessica',
@@ -28,7 +28,7 @@ def test_authorized_dashboard(client):
     json_login_response = login_response.get_json()
     access_token = json_login_response['access_token']
 
-    response = client.get('/dashboard', headers={ "Authorization": f"Bearer {access_token}" })
+    response = client.get('/transactions', headers={ "Authorization": f"Bearer {access_token}" })
     json_response = response.get_json()
     assert json_response["logged_in_as"] == 'jessica@test.com'
 
