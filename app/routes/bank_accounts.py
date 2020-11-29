73c245b8979f3b6d0ddb9e4531a7de64a640a7d2
Gen Ohta <myself@genohta.com>
2020-11-28 18:15:19 -0800

    add case if no bank accounts have been added

--- app/routes/bank_accounts.py
+++ app/routes/bank_accounts.py
@@ -1,12 +1,17 @@
 from flask import Blueprint
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from app import db
-from ..models import Bank, Account
+from ..models import User, Bank, Account
 
 bp = Blueprint('bank_accounts', __name__, url_prefix='/bank_accounts')
 
 @bp.route('/', methods=['GET'], strict_slashes=False)
 @jwt_required
 def bank_accounts():
-    return {}, 501
+    current_user_email = get_jwt_identity()
+    user = User.query.filter_by(email=current_user_email).first()
+    banks = user.banks
+
+    if banks == []:
+        return { 'message': 'No bank accounts have been added yet'}
--- app/tests/routes/test_bank_accounts.py
+++ app/tests/routes/test_bank_accounts.py
@@ -8,3 +8,10 @@ def test_unauthorized_bank_accounts(client):
     response = client.get('/bank_accounts')
     json_response = response.get_json()
     assert json_response['msg'] == 'Missing Authorization Header'
+
+def test_no_bank_accounts(client, login_test_user):
+    access_token = login_test_user
+
+    response = client.get('/bank_accounts', headers={ "Authorization": f"Bearer {access_token}" })
+    json_response = response.get_json()
+    assert json_response["message"] == 'No bank accounts have been added yet'
