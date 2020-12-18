96743f8193e2c691a4d3d5233a239dc0b35b0630
Gen Ohta <myself@genohta.com>
2020-12-17 22:53:55 -0800

    account for fixed costs

--- app/routes/spending_plan_parts.py
+++ app/routes/spending_plan_parts.py
@@ -11,8 +11,17 @@ bp = Blueprint('spending_plan_parts', __name__, url_prefix='/spending_plan_parts
 def spending_plan_parts():
     current_user_email = get_jwt_identity()
     user = User.query.filter_by(email=current_user_email).first()
+
     discretionary_spending_part = SpendingPlanPart.query.filter_by(user=user, category='Discretionary Spending').first()
-    discretionary_spending = { 'id': discretionary_spending_part.id, 'label': discretionary_spending_part.label, 'searchTerm': discretionary_spending_part.search_term, 'expectedAmount': discretionary_spending_part.expected_amount}
-    return { 'discretionarySpending': discretionary_spending}, 200
+    discretionary_spending = { 'id': discretionary_spending_part.id, 'label': discretionary_spending_part.label, 'searchTerm': discretionary_spending_part.search_term, 'expectedAmount': discretionary_spending_part.expected_amount }
+
+    fixed_cost_parts = SpendingPlanPart.query.filter_by(user=user, category='Fixed Costs').order_by('id').all()
+    fixed_costs = list(map(lambda part: { 'id': part.id, 'label': part.label, 'searchTerm': part.search_term, 'expectedAmount': part.expected_amount}, fixed_cost_parts))
+
+    spending_plan = { 'fixedCosts': fixed_costs, 'discretionarySpending': discretionary_spending }
+
+    response = [(category, parts) for category, parts in spending_plan.items() if parts != []]
+
+    return dict(response), 200
 
 
--- app/tests/routes/test_spending_plan_parts.py
+++ app/tests/routes/test_spending_plan_parts.py
@@ -9,23 +9,21 @@ def test_unauthorized_spending_plan_parts(client):
     json_response = response.get_json()
     assert json_response['msg'] == 'Missing Authorization Header'
 
+# TODO make sure the default spending part is added when the user signs up
 def test_default_spending_plan_parts(client, login_test_user):
     access_token = login_test_user
 
     user = User.query.filter_by(email='john@test.com').first()
-    spending_plan_part = SpendingPlanPart(
+    discretionary_spending = SpendingPlanPart(
             category='Discretionary Spending',
             label='Spending Money',
             search_term='*',
             expected_amount=0,
             user=user
             )
-    db.session.add(spending_plan_part)
+    db.session.add(discretionary_spending)
     db.session.commit()
-
-    response = client.get('/spending_plan_parts', headers={"Authorization": f"Bearer {access_token}"})
-    json_response = response.get_json()
-    assert json_response == {
+    expected_response = {
             'discretionarySpending' : {
                 'id': 1,
                 'label': 'Spending Money',
@@ -33,3 +31,113 @@ def test_default_spending_plan_parts(client, login_test_user):
                 'expectedAmount': 0
                 }
             }
+
+
+    response = client.get('/spending_plan_parts', headers={"Authorization": f"Bearer {access_token}"})
+    json_response = response.get_json()
+    assert json_response == expected_response
+
+def test_fixed_costs_spending_plan_parts(client, login_test_user):
+    access_token = login_test_user
+
+    user = User.query.filter_by(email='john@test.com').first()
+    discretionary_spending = SpendingPlanPart(
+            category='Discretionary Spending',
+            label='Spending Money',
+            search_term='*',
+            expected_amount=0,
+            user=user
+            )
+
+    rent = SpendingPlanPart(
+            category='Fixed Costs',
+            label='Rent',
+            search_term='Property Management Company',
+            expected_amount=1000,
+            user=user
+    )
+
+    electricity = SpendingPlanPart(
+            category='Fixed Costs',
+            label='Electricity',
+            search_term='Electic Company',
+            expected_amount=40,
+            user=user
+    )
+
+    gas = SpendingPlanPart(
+            category='Fixed Costs',
+            label='Gas',
+            search_term='Gas Company',
+            expected_amount=40,
+            user=user
+    )
+
+    internet = SpendingPlanPart(
+            category='Fixed Costs',
+            label='Internet',
+            search_term='Internet Provider',
+            expected_amount=60,
+            user=user
+    )
+
+    groceries = SpendingPlanPart(
+            category='Fixed Costs',
+            label='Groceries',
+            search_term='Grocery Store',
+            expected_amount=300,
+            user=user
+    )
+
+    db.session.add(discretionary_spending)
+    db.session.add(rent)
+    db.session.add(electricity)
+    db.session.add(gas)
+    db.session.add(internet)
+    db.session.add(groceries)
+    db.session.commit()
+
+    expected_response = {
+        'fixedCosts': [
+          {
+            'id': 2,
+            'label': 'Rent',
+            'searchTerm': 'Property Management Company',
+            'expectedAmount': 1000
+          },
+          {
+            'id': 3,
+            'label': 'Electricity',
+            'searchTerm': 'Electic Company',
+            'expectedAmount': 40
+          },
+          {
+            'id': 4,
+            'label': 'Gas',
+            'searchTerm': 'Gas Company',
+            'expectedAmount': 40
+          },
+          {
+            'id': 5,
+            'label': 'Internet',
+            'searchTerm': 'Internet Provider',
+            'expectedAmount': 60
+          },
+          {
+            'id': 6,
+            'label': 'Groceries',
+            'searchTerm': 'Grocery Store',
+            'expectedAmount': 300
+          }
+        ],
+        'discretionarySpending': {
+          'id': 1,
+          'label': 'Spending Money',
+          'searchTerm': '*',
+          'expectedAmount': 0
+        }
+      }
+
+    response = client.get('/spending_plan_parts', headers={"Authorization": f"Bearer {access_token}"})
+    json_response = response.get_json()
+    assert json_response == expected_response
