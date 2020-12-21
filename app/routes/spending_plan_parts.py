cf5ad2dd9215df7049047cd08637532b6385c502
Gen Ohta <myself@genohta.com>
2020-12-20 19:47:19 -0800

    add savings

--- app/routes/spending_plan_parts.py
+++ app/routes/spending_plan_parts.py
@@ -13,12 +13,16 @@ def spending_plan_parts():
     user = User.query.filter_by(email=current_user_email).first()
 
     discretionary_spending_part = SpendingPlanPart.query.filter_by(user=user, category='Discretionary Spending').first()
+    # TODO spending plan part to_dict()
     discretionary_spending = { 'id': discretionary_spending_part.id, 'label': discretionary_spending_part.label, 'searchTerm': discretionary_spending_part.search_term, 'expectedAmount': discretionary_spending_part.expected_amount }
 
-    fixed_cost_parts = SpendingPlanPart.query.filter_by(user=user, category='Fixed Costs').order_by('id').all()
-    fixed_costs = list(map(lambda part: { 'id': part.id, 'label': part.label, 'searchTerm': part.search_term, 'expectedAmount': part.expected_amount}, fixed_cost_parts))
+    fixed_costs_parts = SpendingPlanPart.query.filter_by(user=user, category='Fixed Costs').order_by('id').all()
+    fixed_costs = list(map(lambda part: { 'id': part.id, 'label': part.label, 'searchTerm': part.search_term, 'expectedAmount': part.expected_amount}, fixed_costs_parts))
 
-    spending_plan = { 'fixedCosts': fixed_costs, 'discretionarySpending': discretionary_spending }
+    savings_parts = SpendingPlanPart.query.filter_by(user=user, category='Savings').order_by('id').all()
+    savings = list(map(lambda part: { 'id': part.id, 'label': part.label, 'searchTerm': part.search_term, 'expectedAmount': part.expected_amount}, savings_parts))
+
+    spending_plan = { 'fixedCosts': fixed_costs, 'savings': savings, 'discretionarySpending': discretionary_spending }
 
     response = [(category, parts) for category, parts in spending_plan.items() if parts != []]
 
