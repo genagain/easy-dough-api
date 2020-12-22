b99f19ca5d37974cbd534bb5ee8c5ade779d1e85
Gen Ohta <myself@genohta.com>
2020-12-21 20:51:42 -0800

    refactor how the transactions are ingested

--- flask_app.py
+++ flask_app.py
@@ -7,7 +7,7 @@ from flask_bcrypt import generate_password_hash
 from plaid import Client
 
 from app import create_app, db, scheduler, plaid_client
-from app.models import User, Transaction
+from app.models import User, Bank, Transaction
 
 
 app = create_app()
@@ -29,30 +29,33 @@ def add_user():
 
 def print_transactions():
     print('Tick! The time is: %s' % datetime.now())
-    ## TODO make sure access tokens persisted at the user/account level
-    # TODO get this from the each bank
-    access_token = os.getenv('ACCESS_TOKEN')
-    # TODO change this to a day's worth of transactions once I have live data
-    start_date = '{:%Y-%m-%d}'.format(datetime.now() + timedelta(-30))
-    end_date = '{:%Y-%m-%d}'.format(datetime.now())
-    # TODO iterate through each bank
-    # TODO create a transactions class method
-    transactions_response = plaid_client.Transactions.get(access_token, start_date, end_date)
-    transactions_data = transactions_response['transactions']
     with app.app_context():
-        # TODO make sure the transactions are associated to the correct account using the plaid account id
-        # TODO use a try catch block if there is a uniqueness constraint on this column
-        for transaction_datum in transactions_data:
-            date = transaction_datum['date']
-            description = transaction_datum['name']
-            amount = int(transaction_datum['amount']) * 100
-            transaction = Transaction(
-                           date=date,
-                           description=description,
-                           amount=amount
-                           )
-            db.session.add(transaction)
-            db.session.commit()
+        banks = Bank.query.all()
+        for bank in banks:
+            # TODO create accounts by id dict
+            accounts = bank.accounts
+            access_token = bank.access_token
+            start_date = '2020-07-15'
+            end_date = '2020-12-15'
+            # TODO change this to a day's worth of transactions once I have live data
+            # start_date = '{:%Y-%m-%d}'.format(datetime.now() + timedelta(-30))
+            # end_date = '{:%Y-%m-%d}'.format(datetime.now())
+            transactions_response = plaid_client.Transactions.get(access_token, start_date, end_date)
+            transactions_data = transactions_response['transactions']
+            for transaction_datum in transactions_data:
+                # TODO create a transactions class method
+                # TODO make sure the transactions are associated to the correct account using the plaid account id
+                # TODO use a try catch block if there is a uniqueness constraint on this column
+                date = transaction_datum['date']
+                description = transaction_datum['name']
+                amount = int(transaction_datum['amount']) * 100
+                transaction = Transaction(
+                               date=date,
+                               description=description,
+                               amount=amount
+                               )
+                db.session.add(transaction)
+                db.session.commit()
 
 
 scheduler.add_job(add_user, 'cron', hour=1)
