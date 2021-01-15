1f50836d642e1668d4000d9a30ee497145e2c389
Gen Ohta <myself@genohta.com>
2021-01-15 10:02:38 -0800

    do yesterdays transactions

--- flask_app.py
+++ flask_app.py
@@ -19,8 +19,8 @@ def print_transactions():
         for bank in banks:
             accounts_by_id = dict(list(map(lambda account: [account.plaid_account_id, account], bank.accounts)))
             access_token = bank.access_token
-            start_date = '2021-01-01'
-            end_date = '2021-01-13'
+            start_date = '2021-01-14'
+            end_date = '2021-01-14'
             # TODO change this to a day's worth of transactions once I have live data
             # start_date = '{:%Y-%m-%d}'.format(datetime.now() + timedelta(-30))
             # end_date = '{:%Y-%m-%d}'.format(datetime.now())
@@ -49,9 +49,10 @@ def print_transactions():
 
         users = User.query.all()
         for user in users:
-            start_date = '2021-01-01'
-            end_date = '2021-01-13'
+            start_date = '2021-01-14'
+            end_date = '2021-01-14'
             user.categorize_transactions(start_date, end_date)
 
+print_transactions()
 scheduler.add_job(print_transactions, 'cron', hour=2)
 scheduler.start()
