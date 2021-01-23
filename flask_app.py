e28d61eafa13967fc46b9f1b5ce9f6a85859a6cc
Gen Ohta <myself@genohta.com>
2021-01-23 02:10:14 -0800

    ensure no pending transactions are ingested

--- flask_app.py
+++ flask_app.py
@@ -14,18 +14,24 @@ app = create_app()
 
 def print_transactions():
     print('Tick! The time is: %s' % datetime.now())
-    yesterday = '{:%Y-%m-%d}'.format(datetime.now() + timedelta(-1))
+    # yesterday = '{:%Y-%m-%d}'.format(datetime.now() + timedelta(-1))
+    # start_date = '{:%Y-%m-%d}'.format(datetime.now() + timedelta(-2))
+    # end_date = '{:%Y-%m-%d}'.format(datetime.now() + timedelta(-1))
+    start_date = '2021-01-01'
+    end_date = '2021-01-22'
     with app.app_context():
         banks = Bank.query.all()
         for bank in banks:
             accounts_by_id = dict(list(map(lambda account: [account.plaid_account_id, account], bank.accounts)))
             access_token = bank.access_token
-            transactions_response = plaid_client.Transactions.get(access_token, yesterday, yesterday)
+            transactions_response = plaid_client.Transactions.get(access_token, start_date, end_date)
             transactions_data = transactions_response['transactions']
             user = bank.user
             discretionary_spending = SpendingPlanPart.query.filter_by(category="Discretionary Spending", user=user).first()
             for transaction_datum in transactions_data:
                 # TODO create a transactions class method
+                if transaction_datum['pending']:
+                    continue
                 try:
                     date = transaction_datum['date']
                     description = transaction_datum['name']
@@ -45,7 +51,8 @@ def print_transactions():
 
         users = User.query.all()
         for user in users:
-            user.categorize_transactions(yesterday, yesterday)
+            user.categorize_transactions(start_date, end_date)
 
+print_transactions()
 scheduler.add_job(print_transactions, 'cron', hour=2)
 scheduler.start()
